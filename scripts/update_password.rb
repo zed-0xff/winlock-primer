bbb33d4d3410a5f942e466976c26cc82f9f01b1e
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2013-09-15 18:32:05 +0400

    scripts

---
+++ scripts/update_password.rb
@@ -0,0 +1,21 @@
+#coding: utf-8
+# this file must be scheduled run daily at 00:01
+# with enabled option "run immediately if skipped"
+
+require 'digest/md5'
+
+def read_config
+  fname = File.join(File.dirname(__FILE__), "..", "config", "primer.yml")
+  config = {}
+  config = YAML::load_file(fname) if File.exist?(fname)
+  config
+end
+
+user = read_config['user']
+return if !user || user =~ /[\x00-\x20]/ || user[/[\\\/:\|\[\]\{\} ]/]
+
+dt = Time.now
+seed_string = "%02d%02d%04d" % [dt.day, dt.month, dt.year]
+new_password = Digest::MD5.hexdigest(seed_string)[0,8].upcase
+
+system "net user #{config['user']} #{new_password}"
