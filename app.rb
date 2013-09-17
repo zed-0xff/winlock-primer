647280634a4bd79937b4a17b38844d4f726be1b3
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2013-09-18 03:01:07 +0400

    weekends

--- app.rb
+++ app.rb
@@ -12,7 +12,17 @@ def read_config
   fname = File.join(File.dirname(__FILE__), "config", "primer.yml")
   config = {}
   config = YAML::load_file(fname) if File.exist?(fname)
+  if (Time.now.saturday? || Time.now.sunday?) && config['primers_weekend']
+    config['primers'] = config['primers_weekend']
+  end
   config['primers'] ||= 10
+
+  config['modules'] = YAML::load_file(File.join(File.dirname(__FILE__), "data", "index.yml"))
+  config['modules'].each do |k,cfg|
+    raise unless k =~ /\A[a-z0-9_]+\Z/i
+    cfg['data'] = YAML::load_file( File.join(File.dirname(__FILE__), "data", k+".yml") )
+  end
+
   config
 end
 
@@ -29,5 +39,6 @@ get '/' do
     @primers << Primer.generate(100+rand(1900), 6+rand(4))
   end
   @password = Digest::MD5.hexdigest(seed_string)[0,8].upcase
+  @modules = config['modules']
   haml :index
 end
