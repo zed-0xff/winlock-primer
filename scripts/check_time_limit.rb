85e2f9141d96b77bca47859c3e71f526bec77740
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2014-05-28 18:24:49 +0400

    respect summer

--- scripts/check_time_limit.rb
+++ scripts/check_time_limit.rb
@@ -10,9 +10,17 @@ require File.join(File.dirname(__FILE__), "..", "lib", "winapi")
 
 ###############################################################################
 
-# in seconds
-DAILY_LIMIT_WORKDAYS = 3600*2
-DAILY_LIMIT_WEEKENDS = 3600*4
+# all limits are in seconds
+t = Time.now.month*100 + Time.now.day
+if t >= 528 && t <= 902
+  # summer
+  DAILY_LIMIT_WORKDAYS = 3600*4
+  DAILY_LIMIT_WEEKENDS = 3600*4
+else
+  # all other year
+  DAILY_LIMIT_WORKDAYS = 3600*2
+  DAILY_LIMIT_WEEKENDS = 3600*4
+end
 
 ###############################################################################
 
