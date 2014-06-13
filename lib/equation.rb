9227798565233c0d61364fbbf7253f095e17418e
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2014-06-13 10:38:25 +0400

    harder equations

--- lib/equation.rb
+++ lib/equation.rb
@@ -5,29 +5,39 @@ class Equation
   attr_reader :answer, :letter
 
   # exclude letters that look like digits
-  LETTERS = %w'X Y'
+  LETTERS = %w'X Y Z'
 #    %w'A B C D E F G H   J K L M N P Q R S T U V W X Y' +
 #    %w'a b c d e f g h i j k l m n p q r s t u v w x y'
 
   def initialize
-    @letter = LETTERS.random
-    @primer = Primer.new(
-      rand(200), 
-      1, 
-      :actions        => %w'- * * / /',
-      :add_mul_zero   => false,
-      :max_rand_value => 100
-    )
+    @letter  = LETTERS.random
+    @primers = []
+    result = rand(400)
+
+    2.times do
+      @primers << Primer.new(
+        result,
+        2 + rand(2),
+        :actions        => %w'- * * / /',
+        :add_mul_zero   => false,
+        :max_rand_value => 200
+      )
+    end
   end
 
   def to_s
-    ps = @primer.to_s
+    idx = rand(2)
+    ps = @primers[idx].to_s
     matches = []
     ps.scan(/\d+/){ matches << $~ }
     m = matches.random
     ps[m.begin(0)...m.end(0)] = @letter
     @answer = m.to_s.to_i
-    "#{ps} = #{@primer.result}"
+    if idx == 0
+      "#{ps} = #{@primers[1]}"
+    else
+      "#{@primers[0]} = #{ps}"
+    end
   end
 
   def self.generate
